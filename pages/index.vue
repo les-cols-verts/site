1fb4aee4746798aa62adae65ffc6c603050d6f2e
Pierre Recontact <commit@recontact.me>
2020-11-01 19:10:04 +0100

    feat(site): nuxt create app

---
+++ pages/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="container">
+    <div>
+      <!-- <Logo />-->
+      <h1 class="title">
+        Les Cols verts : pollinisateurs d'agriculture urbaine
+      </h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+</style>
