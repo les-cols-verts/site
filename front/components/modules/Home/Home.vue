76262ee3907e2fb84299561b0846cd67ae142613
Pierre Trollé <pierre.trolle@manomano.com>
2020-12-08 14:51:31 +0100

    chore(move): refacto - move components inside folder structure

---
+++ front/components/modules/Home/Home.vue
@@ -0,0 +1,36 @@
+<template>
+  <main class="homepage">
+    <picture>
+      <source
+        media="(min-width:650px)"
+        srcset="../../../static/agriculture_urbaine_xl.jpg"
+      />
+      <img
+        src="../../../static/agriculture_urbaine_xs.jpg"
+        alt="Photo d'urbains dans un jardin"
+        style="width: auto"
+      />
+    </picture>
+    <HomeCta />
+  </main>
+</template>
+
+<style>
+.homepage {
+  margin-top: 20px;
+}
+</style>
+<script>
+import HomeCta from '@/components/modules/Home/HomeCta/HomeCta';
+
+export default {
+  components: { HomeCta },
+};
+</script>
+<style scoped>
+.homepage {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
---
+++ front/components/modules/Home/HomeCta/HomeCta.vue
@@ -0,0 +1,39 @@
+<template>
+  <section class="links">
+    <app-link
+      text="Qui sommes-nous ?"
+      href="https://www.lescolsverts.com/collectifs/"
+    ></app-link>
+    <app-link
+      text="Nos dispositifs d'agriculture urbaine"
+      href="https://www.lescolsverts.com/services/"
+    ></app-link>
+    <app-link
+      text="Découvrir nos actions nationales"
+      href="https://www.lescolsverts.com/collectifs/"
+    ></app-link>
+    <app-link
+      text="Nos actualités"
+      href="https://www.lescolsverts.com/blog/"
+    ></app-link>
+    <app-link
+      text="Nous Contacter"
+      href="https://www.lescolsverts.com/contact/"
+    ></app-link>
+  </section>
+</template>
+<script>
+import AppLink from '@/components/common/Link/AppLink';
+
+export default {
+  name: 'HomeCta',
+  components: { AppLink },
+};
+</script>
+<style scoped>
+.links {
+  display: flex;
+  flex-direction: column;
+  max-width: 500px;
+}
+</style>
