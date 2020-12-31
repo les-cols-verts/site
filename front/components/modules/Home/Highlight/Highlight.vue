649a3b8dbfccbd2312090be38bc7a9dc867e28e2
Pierre Recontact <commit@recontact.me>
2020-12-31 19:06:10 +0100

    feat(highlight): make highlight responsive

--- front/components/modules/Home/Highlight/Highlight.vue
+++ front/components/modules/Home/Highlight/Highlight.vue
@@ -3,7 +3,13 @@
     <a
       href="/mooc"
       class="link"
-      :style="{ backgroundImage: `url(${require('~/static/mooc.jpg')})` }"
+      :style="{
+        backgroundImage: `url(${
+          IS_DESKTOP
+            ? require('~/static/mooc_xl.jpg')
+            : require('~/static/mooc_xs.jpg')
+        })`,
+      }"
     >
       <span class="tile__overlay">
         <span class="tile__button button button--lowercase button--primary">
@@ -14,7 +20,13 @@
     <a
       href="/incubateur"
       class="link"
-      :style="{ backgroundImage: `url(${require('~/static/incub.jpg')})` }"
+      :style="{
+        backgroundImage: `url(${
+          IS_DESKTOP
+            ? require('~/static/incub_xl.jpg')
+            : require('~/static/incub_xs.jpg')
+        })`,
+      }"
     >
       <span class="tile__overlay">
         <span class="tile__button button button--lowercase button--primary">
@@ -25,26 +37,26 @@
   </div>
 </template>
 <script>
+import { IS_DESKTOP } from '@/components/utils/responsive/responsive-utils';
+
 export default {
   name: 'Highlight',
+  computed: {
+    IS_DESKTOP,
+  },
 };
 </script>
 <style lang="scss" scoped>
 .highlight {
   display: flex;
-  flex-direction: row;
+  flex-direction: column;
   margin-top: 20px;
 }
 
 .link {
-  width: 595px;
-  height: 333px;
-  margin: 20px;
-}
-
-.tile__overlay:hover {
-  background-color: rgba(0, 0, 0, 0.6);
-  transition: background-color 1s;
+  width: 298px;
+  height: 167px;
+  margin-bottom: 20px;
 }
 
 .tile__overlay {
@@ -54,23 +66,39 @@ export default {
   width: 100%;
 }
 
-.link:hover .tile__button {
-  transform: translateY(600%);
-  transition: transform 0.5s;
-}
-
 .tile__button {
   position: absolute;
   display: table;
   background: $darkGreen;
   color: $white;
   padding: 0.8em 1.5em;
-  margin: -50px auto auto;
+  margin: 120px auto auto;
+  font-size: 0.7em;
 }
 
-/*@media only screen and (min-width: 1024px) {*/
-/*  .link {*/
-/*    width: 45%;*/
-/*  }*/
-/*}*/
+@media only screen and (min-width: 1024px) {
+  .highlight {
+    flex-direction: row;
+  }
+
+  .link {
+    width: 595px;
+    height: 333px;
+    margin: 20px;
+  }
+
+  .tile__button {
+    margin: -50px auto auto;
+  }
+
+  .link:hover .tile__button {
+    transform: translateY(600%);
+    transition: transform 0.5s;
+  }
+
+  .tile__overlay:hover {
+    background-color: rgba(0, 0, 0, 0.6);
+    transition: background-color 1s;
+  }
+}
 </style>
---
+++ front/components/utils/screen/screen-utils.js
@@ -0,0 +1,25 @@
+const isBrowser = typeof window !== 'undefined';
+
+const screenWidth = () => {
+  if (!isBrowser) {
+    return '1000';
+  }
+  return (
+    window.innerWidth ||
+    document.documentElement.clientWidth ||
+    document.body.clientWidth
+  );
+};
+
+const screenHeight = () => {
+  if (!isBrowser) {
+    return '600';
+  }
+  return (
+    window.innerHeight ||
+    document.documentElement.clientHeight ||
+    document.body.clientHeight
+  );
+};
+
+export { screenWidth, screenHeight };
